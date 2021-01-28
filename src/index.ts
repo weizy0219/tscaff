ad1cb7513939494490aebae93a89992e2dff6918
weizy0219 <weizy0219@users.noreply.github.com>
2021-01-28 22:12:12 +0800

    first commit

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,8 @@
-import * as clipboardy from 'clipboardy';
 const sname = 5;
 let a: number;
 
 export function HelloWorld(){
     return 'Hello World!'
 }
-const autoClip=()=>{
-    // clipboardy.writeSync('🦄');
-    return(typeof clipboardy.readSync());
-}
 
-console.log(autoClip());
+console.log(HelloWorld());
